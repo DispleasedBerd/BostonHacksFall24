65e9b2eb386a130f935bce7658f05754b2bcd321
rhea <rthakur@bu.edu>
2024-11-02 17:53:37 -0400

    adding

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './logo.svg'; // Adjust path as necessary
+import logo2 from './logo2.png'; // Your second logo if needed
 import './App.css';
 
 function Header() {
   return (
     <div className="Header">
+       <img src={logo2} alt="Urban Refuge Logo" className="Logo" /> {/* Add the logo */}
       <h1>Urban Refuge</h1> {/* Name of Website */}
       <nav>
         <ul>
-          {/* Used for the headings of the header bar */}
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact</a></li>
-          <li><a href="#contact">User Login</a></li>
+          <li><a href="#login">User Login</a></li>
         </ul>
       </nav>
     </div>
   );
 }
 
-
 function App() {
   return (
     <div className="App">
       <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       {/*} <img src={logo2} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -42,4 +43,3 @@ function App() {
 }
 
 export default App;
-

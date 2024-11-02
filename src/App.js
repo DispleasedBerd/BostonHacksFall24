2a38367d713eeb13beeb8db0c1ddce8a8d03436f
Alexander Smeulders <alexs23@bu.edu>
2024-11-02 17:51:19 -0400

    map inserted

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function Header() {
-  return (
-    <div className="Header">
-      <h1>Urban Refuge</h1> {/* Name of Website */}
-      <nav>
-        <ul>
-          {/* Used for the headings of the header bar */}
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-          <li><a href="#contact">User Login</a></li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
-
+/* installs
+1. npm install react react-dom leaflet
+2. npm install react-leaflet
+3. npm install -D @types/leaflet
+*/
+import './App.css'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css"
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <MapContainer center={[42.3601, -71.0589]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[42.26861281372227, -71.09347770697872]}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+    </MapContainer>
+
+    );
 }
 
 export default App;
